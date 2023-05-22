dd6b1866b14b4a2adde65dda49d3520f5bc5344e
K-Pinck <72100851+K-Pinck@users.noreply.github.com>
2023-05-22 13:58:06 -0500

    Keith update

--- .nuxt/dist/server/server.mjs
+++
@@ -1,1 +0,0 @@
-export { default } from "file:///Users/williamalexy/work/securedmoving.us/node_modules/@nuxt/vite-builder/dist/runtime/vite-node.mjs"
--- components/PaginationLink.ts
+++
@@ -1,4 +0,0 @@
-export default defineNuxtLink({
-    componentName: "PaginationLink",
-    prefetchedClass:"inline-flex items-center border-t-2 border-transparent px-4 pt-4 text-sm font-medium  hover:border-stone-300 hover:text-stone-600 text-stone-700"
-  });
