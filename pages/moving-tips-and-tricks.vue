c25dd26fb754e74ea44ee2085410b2837a7dea12
K-Pinck <72100851+K-Pinck@users.noreply.github.com>
2023-05-30 13:06:37 -0500

    page completion

--- pages/moving-tips-and-tricks.vue
+++ pages/moving-tips-and-tricks.vue
@@ -2,7 +2,8 @@
   <div>
     <NuxtLayout>
       <MainNav></MainNav>
-      <section class="choose-hero_section w-full">
+      <!-- Begin hero -->
+      <section class="moving-tips_hero_section w-full">
         <div
           class="moving-hero_heading-wrap max-w-7xl text-center py-5 pt-20 md:pt-40 md:py-20 mx-auto"
         >
@@ -23,69 +24,50 @@
           </h2>
         </div>
       </section>
-      <!-- Begin packing -->
-      <section class="packing-like_section w-full border py-11">
-        <!-- Begin Packing-->
+      <!-- End hero -->
+      <!-- Begin main -->
+      <section class="packing-main_section w-full py-11">
         <div
-          class="packing-content_wrap w-11/12 md:max-w-7xl mx-auto p-5 flex flex-col-reverse md:flex-row items-center bg-gray-50 border rounded-md"
+          v-for="wrap in wraps"
+          class="packing-content_wrap w-11/12 md:max-w-7xl mx-auto mb-6 p-5 flex flex-col-reverse md:flex-row items-center bg-gray-100 rounded-md"
         >
-          <div class="col-left md:w-3/5 p-2 md:p-6 border">
-            <h2 class="font-bold leading-8 text-xl">Packing Like a Pro:</h2>
-            <p>
-              Learn the art of efficient packing with our expert tips. Discover
-              strategies to streamline the packing process, including
-              room-by-room packing, labeling techniques, and utilizing packing
-              supplies effectively. We'll help you pack your belongings securely
-              and stay organized throughout the move.
-            </p>
+          <div class="col-left md:w-3/5 p-2 md:p-6">
+            <h2 class="font-bold leading-8 text-xl">{{ wrap.name }}</h2>
+            <p>{{ wrap.description }}</p>
           </div>
-          <div class="col-right md:w-2/5 border">
-            <nuxt-img
-              provider="cloudinary"
-              src="a19_20221207_092625-jpg_2022-12-07_2219/20221207_084830_gmbxhl"
-              class="md:w-full md:h-full object-contain rounded-md"
-            />
-          </div>
-          <!-- End Packing-->
-        </div>
-      </section>
-      <!-- End packing -->
-      <!-- <section class="choose-main w-full mx-auto py-20 bg-orange-50">
-        <div class="choose-two-col max-w-6xl mx-auto px-8">
-          <dl class="space-y-10 lg:space-y-0 lg:grid lg:grid-cols-2 lg:gap-8">
-            <div v-for="feature in features" :key="feature.name">
-              <dt>
-                <p class="mt-5 text-xl leading-6 font-medium">
-                  {{ feature.name }}
-                </p>
-              </dt>
-              <dd class="mt-2 text-base text-gray-900">
-                {{ feature.description }}
-              </dd>
+          <div class="col-right md:w-2/5">
+            <div class="img-wrap w-auto h-auto">
+              <nuxt-img
+                provider="cloudinary"
+                :src="wrap.imgUrl"
+                class="md:w-full md:h-full object-cover rounded-md"
+              />
             </div>
-          </dl>
+          </div>
         </div>
       </section>
-      <section class="services-cta w-full bg-gray-900 py-14">
+      <!-- End main-->
+      <!-- Begin CTA -->
+      <section class="services-cta w-full bg-orange-50 py-14">
         <div
-          class="cta-wrap max-w-7xl mx-auto flex flex-col items-center gap-4"
+          class="cta-wrap w-9/12 md:max-w-7xl mx-auto flex flex-col items-center gap-4"
         >
-          <h2
-            class="text-4xl w-11/12 text-white text-center font-bold md:w-full"
-          >
-            Ready to experience the difference with Secured Moving Company?
+          <h2 class="text-4xl 11/12 md:w-9/12 text-center font-bold">
+            Ready to dive into our comprehensive Moving Tips and Tricks guide?
           </h2>
-          <p class="text-lg text-white text-center w-11/12 md:w-9/12 font-bold">
-            Get in touch with us today for a free, no-obligation quote. Let us
-            show you why we are the trusted choice for hassle-free moves.
+          <p class="text-lg w-11/12 md:w-9/12 font-bold">
+            Explore each section in detail and equip yourself with the knowledge
+            and strategies to make your move a smooth and stress-free
+            experience.
           </p>
           <a
-            href="/contact-us"
+            href="https://www.securedmoving.com/fort-worth-moving-packing"
             class="px-8 py-4 text-white text-center mx-auto font-bold w-11/12 md:w-1/4 bg-secureRed-700 rounded-md"
             >Get A Quote</a
           >
         </div>
-      </section> -->
+      </section>
+      <!-- End CTA -->
     </NuxtLayout>
   </div>
 </template>
@@ -96,42 +78,55 @@ import {
   ScaleIcon,
 } from "@heroicons/vue/24/outline/index.js";
 
-const features = [
+const wraps = [
   {
-    name: "Reliability and Experience:",
+    name: "Packing Like a Pro",
     description:
-      "With years of experience in the industry, we have built a reputation for reliability and professionalism. Our skilled team has successfully completed numerous moves, ensuring that each customer's belongings are handled with care and delivered safely to their new destination.",
-    icon: GlobeAltIcon,
+      "Learn the art of efficient packing with our expert tips. Discover strategies to streamline the packing process, including room-by-room packing, labeling techniques, and utilizing packing supplies effectively. We'll help you pack your belongings securely and stay organized throughout the move.",
+    imgUrl: "packing-services/20221120_123141_orcbyr",
   },
   {
-    name: "Professionalism and Trained Staff:",
+    name: "Decluttering and Downsizing:",
     description:
-      "At Secured Moving Company, we take pride in our team of trained professionals. Our movers are highly skilled and undergo thorough training to handle all aspects of the move efficiently and professionally. You can trust us to treat your belongings with the utmost care and respect.",
-    icon: ScaleIcon,
+      "Before you move, take the opportunity to declutter and downsize your belongings. Our guide provides practical tips on sorting through your items, deciding what to keep, donate, or discard. Lighten your load and start fresh in your new space.",
+    imgUrl: "Packed-Boxes-Main_wn3olq",
   },
   {
-    name: "Customer Service Excellence:",
+    name: "Moving Day Essentials:",
     description:
-      "We prioritize exceptional customer service and strive to exceed your expectations. Our friendly and knowledgeable team is here to address your concerns, answer your questions, and provide personalized assistance throughout the moving process. We believe in open communication and prompt responses to ensure your peace of mind. ",
-    icon: BoltIcon,
+      "Be prepared for moving day with our checklist of essentials. We'll guide you on what to pack in your 'moving day' box, including important documents, essential toiletries, and basic tools. Having these essentials readily accessible will help you navigate the day smoothly.",
+    imgUrl: "packing-services/20221120_123538_1_tivpul",
   },
   {
-    name: "Licensed and Insured:",
+    name: "Managing Logistics:",
     description:
-      "Secured Moving Company is fully licensed and insured. This means that your belongings are protected during the entire moving process. You can have confidence knowing that your move is in the hands of a reputable and responsible company.",
-    icon: BoltIcon,
+      "Managing logistics is key to a successful move. Our guide offers tips on coordinating with moving companies, creating a moving timeline, and notifying important parties, such as utility providers and change of address notifications. Stay organized and ensure a seamless transition.",
+    imgUrl: "packing-services/20221120_125008_d2eqnm.jpg",
   },
   {
-    name: "Transparent Pricing:",
+    name: "Unpacking and Settling In:",
     description:
-      "We believe in transparent pricing, providing accurate and detailed quotes without any hidden fees. Our pricing structure is fair and competitive, reflecting the quality of our services. You can trust that the quote provided is accurate, allowing you to plan your budget effectively.",
-    icon: BoltIcon,
+      "Once you arrive at your new home, our tips will help you unpack and settle in efficiently. From prioritizing unpacking to creating an organized system, we'll guide you through the process of making your new space feel like home.",
+    imgUrl: "organized-drawer-for-move-p-1080_ymkzy6.jpg",
   },
   {
-    name: "Care for Valuables and Fragile Items:",
+    name: "Stress Relief and Self-Care:",
     description:
-      "We understand the importance of your valuables and fragile items. Our team has the expertise and experience to handle delicate belongings, such as pianos, artwork, or antiques. We take special care to ensure they are properly protected and transported with utmost care.",
-    icon: BoltIcon,
+      "Moving can be stressful, but it's essential to take care of yourself during the process. Our guide provides self-care tips and stress-relief techniques to help you stay grounded and maintain your well-being throughout the move. Remember to take breaks and practice self-care.",
+    imgUrl: "Saqinaw_Social_7c551144a2",
+  },
+  {
+    name: "Moving with Kids and Pets:",
+    description:
+      "Moving with kids and pets can present unique challenges. Our guide offers practical strategies to ease the transition for your little ones and furry friends. From involving them in the process to providing them with comfort and security, we'll help you navigate moving with your loved ones.",
+    imgUrl: "jiawei-zhao-BsXeYX3efOI-unsplash_o3d8fa",
+  },
+  {
+    name: "Environmental-Friendly Moving Tips:",
+    description:
+      "We believe in sustainable moving practices. Our guide includes eco-friendly tips and practices to minimize waste, recycle packing materials, and choose environmentally friendly options. Make your move not only efficient but also environmentally conscious.",
+    imgUrl:
+      "61d86d41bb053d5011e6596a_gerhard-crous-blue-heron-keller-tx_hyugdh",
   },
 ];
 </script>
